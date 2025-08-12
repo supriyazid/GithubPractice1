cc2371af49be24389b4ce41eba994254065a30e9
supriyazid <supriyazid@gmail.com>
2025-08-12 15:27:16 +0530

    Updated

--- src/Pages/homepageP.java
+++ src/Pages/homepageP.java
@@ -4,7 +4,7 @@ public class homepageP {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("welcome page");
+System.out.println("welcome updatednow page");
 	}
 
 }
